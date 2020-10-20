d315f08503d4765082588b998379c49531a6832d
derrick-aung <derrick.aungkhant@gmail.com>
2020-10-21 01:26:47 +0630

    Original question attached

--- questionOne.js
+++ questionOne.js
@@ -1,3 +1,18 @@
+/*
+Question 1 
+Create a method that takes a URL (string), removes duplicate URL query parameters and they are specified within the 2nd argument (which will be an optional array).
+
+Examples 
+stripUrlParams("https://google.com?a=1&b=2&a=2") ➞ "https://google.com?a=2&b=2"
+stripUrlParams("https://google.com?a=1&b=2&a=2", ["b"]) ➞ "https://google.com?a=2"
+stripUrlParams("https://google.com", ["b"]) ➞ "https://google.com"
+
+Notes:
+- The 2nd argument paramsToStrip is optional.
+- paramsToStrip can contain multiple params.
+- If there are duplicate query parameters with different values, use the value of the last occurring parameter (see examples #1 and #2 above). 
+*/
+
 const stripUrlParams = (urlString, paramsToStrip = []) => {
 	let newParams = {};
 	const [url, params] = urlString.split("?");
